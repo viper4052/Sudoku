3ce6a317917dd1c45242461f40263527125f42ce
vpier4052 <viper4052@hotmail.com>
2019-11-21 23:49:56 -0600

    se agregaron nuevos tableros de partida e interfaz de menu configuracion

--- sudoku_carlos_barrantes.py
+++ sudoku_carlos_barrantes.py
@@ -2,6 +2,7 @@
 from tkinter import *
 from tkinter import font
 from tkinter import messagebox
+from tkinter import ttk
 import random
 from time import *
 import os
@@ -21,7 +22,6 @@ for i in range(0, 21):
     ventana.columnconfigure(i, weight=1)
     ventana.rowconfigure(i, weight=1)
 
-
 # ----------------- ICONO -----------------------------
 ventana.iconbitmap('sudoku.ico')
 
@@ -29,6 +29,11 @@ ventana.iconbitmap('sudoku.ico')
 # ----------------- FUENTE GLOBAL -----------------------------
 CalibriL = font.Font(family='Calibri Light', size=8, weight='bold')
 
+# ------------------- BOOLEANOS GLOBALES ----------------------
+var_timer = False
+dific_facil = False
+dific_inter = False
+dific_dificil = False
 
 # ------------------------ TITULO --------------------------
 l_sudoku = Label(ventana, text='SUDOKU', bg="#b2bec3",
@@ -36,24 +41,63 @@ l_sudoku = Label(ventana, text='SUDOKU', bg="#b2bec3",
 l_sudoku.grid(column=10, row=0, columnspan=1)
 
 # --------------------- DICCIONARIOS DE PARTIDAS -------------------
-
-dicc_partida_facil = {}
-dicc_partida_intermedio = {}
+# OJO GRABAR EN ARCHIVO Y LEER DESDE AHI
+dicc_partida_facil = {1: [
+    ['5', '3', '', '', '7', '', '', '', ''],
+    ['6', '', '', '1', '9', '5', '', '', ''],
+    ['', '9', '8', '', '', '', '', '6', ''],
+    ['8', '', '', '', '6', '', '', '', '3'],
+    ['4', '', '', '8', '', '3', '', '', '1'],
+    ['7', '', '', '', '2', '', '', '', '6'],
+    ['', '6', '', '', '', '', '2', '8', ''],
+    ['', '', '', '4', '1', '9', '', '', '5'],
+    ['', '', '', '', '8', '', '', '7', '9']]}
+# OJO GRABAR EN ARCHIVO Y LEER DESDE AHI
+dicc_partida_intermedio = {1: [
+    ['', '', '', '', '8', '', '', '', '6'],
+    ['', '9', '4', '', '5', '', '', '', ''],
+    ['8', '', '2', '', '', '3', '', '', ''],
+    ['', '', '', '8', '3', '', '7', '5', ''],
+    ['', '', '', '7', '4', '', '6', '9', ''],
+    ['', '3', '', '1', '', '6', '', '', '2'],
+    ['', '6', '', '', '', '', '2', '8', ''],
+    ['', '', '', '4', '1', '9', '', '', '5'],
+    ['3', '4', '', '', '6', '', '8', '2', '']]}
+# OJO GRABAR EN ARCHIVO Y LEER DESDE AHI
 dicc_partida_dificil = {}
+
 
 # --------------------- MARCO DE BOTONES --------------------
 
 def zona_de_juego():
+    global dific_facil, dific_inter
+    probabilidad = random.randint(0, 100)
+    # prueba de muestra de tablero
+    tablero = 1
+    if dific_facil == True:
+        if probabilidad < 50:
+            tablero = 1
+        else:
+            tablero = 2
+        # agregar condicional para el tercer sudoku
+    elif dific_inter == True:
+        if probabilidad < 50:
+            tablero = 4
+        else:
+            tablero = 5
     marco_juego = Frame(ventana, width=250, height=250)
     marco_juego.configure(bg='white')
     for i in range(9):
         marco_juego.columnconfigure(i, weight=1)
         marco_juego.rowconfigure(i, weight=1)
+    marco_juego.grid(column=3, row=1, sticky=NSEW, columnspan=9, rowspan=9)
+    dict = eval(open('sudoku2019partidas.dat').read())
     for x in range(9):
         for y in range(9):
-            btn_celda = Button(marco_juego)
+            btn_celda = Button(marco_juego, text=dict[tablero][y][x])
+            if dict[tablero][y][x] != '':
+                btn_celda.config(state=DISABLED)
             btn_celda.grid(column=x, row=y, sticky=N+S+E+W)
-    marco_juego.grid(column=3, row=1, sticky=NSEW, columnspan=9, rowspan=9)
 
 
 def botones_ventana():
@@ -144,6 +188,92 @@ def menu_jugar():
     botones_ventana()
     entry_jugador()
 
+def menu_configurar():
+    global dific_facil, dific_inter, dific_dificil
+    ANCHO2, ALTO2 = 300, 320
+    POS_VENTANA_X2, POS_VENTANA_Y2 = 650, 400
+    ventanaConfiguracion = Toplevel()
+    ventanaConfiguracion.title('Configuración')
+    ventanaConfiguracion.geometry('{}x{}+{}+{}'.format(ANCHO2, ALTO2,
+                                                       POS_VENTANA_X2, POS_VENTANA_Y2))
+    ventanaConfiguracion.resizable(width=False, height=False)
+    for i in range(50):
+        ventanaConfiguracion.columnconfigure(i, weight=1)
+        ventanaConfiguracion.rowconfigure(i, weight=1)
+
+    nb = ttk.Notebook(ventanaConfiguracion)
+    nb.grid(row=1, column=0, columnspan=51, rowspan=49, sticky='NESW')
+
+    # Pagina 1 de config
+    pagina1 = ttk.Frame(nb)
+    nb.add(pagina1, text='Nivel')
+    facil_rb = Radiobutton(pagina1, text='Fácil', value='1', variable=1)
+    facil_rb.place(x=50, y=80)
+    intermedio_rb = Radiobutton(pagina1, text='Intermedio', value='2', variable=2)
+    intermedio_rb.place(x=50, y=110)
+    dificil_rb = Radiobutton(pagina1, text='Difícil', value='3', variable=3)
+    dificil_rb.place(x=50, y=140)
+
+    # Pagina 2 de config
+    pagina2 = ttk.Frame(nb)
+    nb.add(pagina2, text='Reloj')
+    btn_si = StringVar()
+    btn_no = StringVar()
+    btn_timer = StringVar()
+    si_rb = Radiobutton(pagina2, text='Sí', value=4)#, variable=btn_si)
+    si_rb.place(x=50, y=80)
+    no_rb = Radiobutton(pagina2, text='No', value=5)#, variable=btn_no)
+    no_rb.place(x=50, y=110)
+    timer_rb = Radiobutton(pagina2, text='Timer', value=6)#, variable=btn_timer)
+    timer_rb.place(x=50, y=140)
+
+    #def guarda_cambios():
+     #   x = btn_si.get()
+     #   y = btn_no.get()
+     #   z = btn_timer.get()
+     #   print(x, y, z)
+
+    l_tiempo_pref = Label(pagina2, text='HH:MM:SS', font=(CalibriL, 8))
+    l_tiempo_pref.place(x=185, y=75)
+
+    horas_str = StringVar()
+    horas = Entry(pagina2, width=3, font=(CalibriL, 10), bg='azure',
+                  textvar=horas_str, justify='left')
+    horas.place(x=170, y=100)
+
+    minut_str = StringVar()
+    minut = Entry(pagina2, width=3, font=(CalibriL, 10), bg='azure',
+                  textvar=minut_str, justify='left')
+    minut.place(x=200, y=100)
+
+    seg_str = StringVar()
+    segundos = Entry(pagina2, width=3, font=(CalibriL, 10), bg='azure',
+                  textvar=seg_str, justify='left')
+    segundos.place(x=230, y=100)
+
+    # Pagina 3 de config
+    pagina3 = ttk.Frame(nb)
+    nb.add(pagina3, text='Elementos')
+
+    def cambia_dificultad():
+        global dific_facil, dific_inter, dific_dificil
+        if facil_rb:
+            pass
+
+    # --------------------- BOTON APLICAR CONFIGURACION EN CADA PESTANA
+    boton_aplicar_dif = Button(pagina1, text='Aplicar', command=None)
+    boton_aplicar_dif.place(x=240, y=250)
+
+    boton_aplicar_reloj = Button(pagina2, text='Aplicar', command=None)
+    boton_aplicar_reloj.place(x=240, y=250)
+
+    boton_aplicar_elem = Button(pagina3, text='Aplicar', command=None)
+    boton_aplicar_elem.place(x=240, y=250)
+
+    ventanaConfiguracion.transient()
+    ventanaConfiguracion.grab_set()
+    ventana.wait_window(ventanaConfiguracion)
+
 
 def acerca_de():
     messagebox.showinfo(message="SUDOKU /// "
@@ -167,19 +297,13 @@ ventana.config(menu=menubar)
 
 menubar.add_cascade(label='Jugar', command=menu_jugar)
 
-menubar.add_cascade(label='Configuracion', command=None)
+menubar.add_cascade(label='Configuracion', command=menu_configurar)
 
 menubar.add_cascade(label='Acerca de', command=acerca_de)
 
 menubar.add_cascade(label='Ayuda', command=None)
 
 menubar.add_cascade(label='Salir', command=salir)
-
-
-
-
-
-
 
 ventana.mainloop()
 
